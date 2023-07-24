83b4474651ccaf11257d6946b32ce07c0cffab32
Mikael Melo <mikaelsilva1940@hotmail.com>
2023-07-24 19:28:01 -0300

    ajustado o localstorage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
-
+const itens = [];
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
-  crieElement(
-    evento.target.elements["nome"].value,
-    evento.target.elements["quantidade"].value
-  );
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+
+  crieElement(nome.value, quantidade.value);
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function crieElement(nome, quantidade) {
@@ -16,10 +18,18 @@ function crieElement(nome, quantidade) {
 
   const numeroItem = document.createElement("strong");
   numeroItem.innerHTML = quantidade;
-
   novoItem.appendChild(numeroItem);
 
   novoItem.innerHTML += nome;
 
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("item", JSON.stringify(itens));
 }
